7d8a6fbdefe7d25e8ce174b12f1d354c96f3ddc3
Shubhabrata Ghosh <shubhabrata.g1108@gmail.com>
2025-01-31 00:37:35 +0530

    2019 Puzzle 1, Part 2 done

--- 2019/puzzle1/solution.js
+++ 2019/puzzle1/solution.js
@@ -1,12 +1,17 @@
 import fs from "node:fs";
 
+const recursiveFuelRequirement = (mass) => {
+  const fuel = Math.floor(Number(mass) / 3) - 2;
+  return fuel <= 0 ? 0 : fuel + recursiveFuelRequirement(fuel);
+};
+
 const getRequiredFuel = (input) => {
   const ipArray = input.split("\n");
   return ipArray.reduce((reqFuel, mass) => {
-    const fuel = Math.floor(Number(mass) / 3) - 2;
-    return reqFuel + fuel;
+    return reqFuel + recursiveFuelRequirement(mass);
   }, 0);
 };
+
 
 const ipContent = fs.readFileSync("./probleminput.txt", "utf8");
 console.log("Fuel required: ", getRequiredFuel(ipContent));
